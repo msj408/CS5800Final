b17d3637060c1c37bfd1cbac024093353e3a834c
msj408 <manbir410@gmail.com>
2024-04-22 22:44:16 -0700

    update 4/22

--- src/EventSearch.jsx
+++ src/EventSearch.jsx
@@ -3,18 +3,25 @@ import axios from "axios";
 import EventCard from "./components/EventCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Select from "react-select";
-import DatePicker from "react-datepicker";
+import DatePicker from "react-datepicker"; // Import DatePicker
+import "react-datepicker/dist/react-datepicker.css"; // Import CSS for DatePicker
+import GooglePlacesAutocomplete from "react-google-autocomplete";
+
 const TicketmasterForm = () => {
   const apiKey = "o1U6AskSchLZSkIvnoZpAQIOi7q7APGh";
   const [keyword, setKeyword] = useState("");
   const [city, setCity] = useState("");
-  const [startDateTime, setStartDateTime] = useState(new Date());
+  const [googleCity, setGoogleCity] = useState("");
   const [countryCode, setCountryCode] = useState("US"); // Default to US
   const [results, setResults] = useState([]);
   const [size, setSize] = useState([]);
   const [radius, setRadius] = useState([]);
   const [selectedGenres, setSelectedGenres] = useState([]);
   const [selectedArtists, setSelectedArtists] = useState([]);
+  const [isEventSelected, setIsEventSelected] = useState(false);
+  const [selectedAddress, setSelectedAddress] = useState("");
+  // const [startDate, setStartDate] = useState(new Date("2024-05-25"));
+
   const favoriteArtists = [
     { value: "artist1", label: "Taylor Swift" },
     { value: "artist2", label: "Beyonce" },
@@ -36,7 +43,6 @@ const TicketmasterForm = () => {
       countryCode,
       size,
       radius,
-      startDateTime,
     };
     const headers = { "Access-Control-Allow-Origin": "*" };
 
@@ -47,10 +53,49 @@ const TicketmasterForm = () => {
       console.error("Error fetching events:", error);
     }
   };
-  console.log(results);
+  //   try {
+  //     const response = await axios.get(apiUrl, { params }, { headers });
+  //     // const filteredEvents = response.data._embedded?.events.filter((event) => {
+  //     //   // Convert event start date to a Date object
+  //     //   const eventStartDate = new Date(event.dates.start.localDate);
+  //     //   // Convert the selected start date to a Date object
+  //     //   const selectedStartDate = new Date(startDate);
+  //     //   // Compare the event start date with the selected start date
+  //     //   return eventStartDate.getTime() === selectedStartDate.getTime();
+  //     // });
+  //     setResults(filteredEvents || []);
+  //   } catch (error) {
+  //     console.error("Error fetching events:", error);
+  //   }
+  // };
+  // console.log(results);
+  const handleEventSelect = () => {
+    setIsEventSelected(true);
+  };
+  const handleAddressSelect = (address) => {
+    setSelectedAddress(address);
+  };
+  const handleCalculateShortestPath = () => {
+    alert(
+      `Ending Address: ${selectedAddress}, Starting Address: ${googleCity}`
+    );
+  };
 
   return (
     <div>
+      <div>
+        <h2>MY Location</h2>
+        <label htmlFor="city">City:</label>
+        <GooglePlacesAutocomplete
+          apiKey="AIzaSyD8pBnMosh59Ix2-EriTkgP0XO-yvoQQCw"
+          onSelect={(place) => setGoogleCity(place.formatted_address)}
+          placeholder="Enter city"
+          inputStyle={{ width: "100%" }}
+          autocompletionRequest={{
+            types: ["(cities)"],
+          }}
+        />
+      </div>
       <h2>Ticketmaster Event Search</h2>
       <form onSubmit={handleSubmit}>
         <div>
@@ -98,6 +143,15 @@ const TicketmasterForm = () => {
             onChange={(e) => setRadius(e.target.value)}
           />
         </div>
+        {/* <div>
+          <label htmlFor="startDate">Start Date:</label>
+          <DatePicker // Use DatePicker component
+            id="startDate"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            dateFormat="yyyy-MM-dd"
+          />
+        </div> */}
         <h2>My Preferences</h2>
         <label htmlFor="FavoriteArtists">Favorite Artists:</label>
         <Select
@@ -119,7 +173,13 @@ const TicketmasterForm = () => {
         />
         <button type="submit">Search Events</button>
       </form>
-
+      <div>
+        {isEventSelected && (
+          <button onClick={handleCalculateShortestPath}>
+            Calculate Shortest Path
+          </button>
+        )}
+      </div>
       <div className="container">
         {" "}
         <div className="row">
@@ -127,7 +187,11 @@ const TicketmasterForm = () => {
           {results.length > 0 ? (
             results.map((event) => (
               <div className="col-md-4 mb-3" key={event.id}>
-                <EventCard event={event} />
+                <EventCard
+                  event={event}
+                  onSelect={handleEventSelect}
+                  onAddressSelect={handleAddressSelect}
+                />
               </div>
             ))
           ) : (
--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const EventCard = ({ event }) => {
-  const formatTime = (timeString) => {
-    let [hours, minutes] = timeString?.split(":");
-    let ampm = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12;
-    hours = hours ? hours : 12; // 12 instead of 0
-    minutes = minutes < 10 && minutes != "00" ? "0" + minutes : minutes;
-    return hours + ":" + minutes + " " + ampm;
+const EventCard = ({ event, onSelect }) => {
+  const [selectedAddress, setSelectedAddress] = useState(null);
+
+  // const formatTime = (timeString) => {
+  //   let [hours, minutes] = timeString?.split(":");
+  //   let ampm = hours >= 12 ? "PM" : "AM";
+  //   hours = hours % 12;
+  //   hours = hours ? hours : 12; // 12 instead of 0
+  //   minutes = minutes < 10 && minutes !== "00" ? "0" + minutes : minutes;
+  //   return hours + ":" + minutes + " " + ampm;
+  // };
+
+  const handleSelectAddress = () => {
+    // Get the address associated with the event
+    const address =
+      event._embedded.venues[0].address.line1 +
+      ", " +
+      event._embedded.venues[0].city.name +
+      ", " +
+      event._embedded.venues[0].state.stateCode;
+    setSelectedAddress(address);
+    onSelect();
+    onAddressSelect(address);
   };
+
   return (
-    <div className="event-card">
-      <img width={"250px"} src={event.images[4].url}></img>
+    <div className={selectedAddress ? "selected event-card" : "event-card"}>
+      <img width={"250px"} src={event.images[4].url} alt={event.name} />
       <h3>{event.name}</h3>
       <p>
         <strong>Date:</strong> {event.dates.start.localDate}
       </p>
+      {/* 
       <p>
         <strong>Time:</strong>{" "}
         {event.dates.start.localTime
           ? formatTime(event.dates.start.localTime)
           : event.dates.start.localTime}
-      </p>
+      </p> */}
       <p>
         <strong>Venue:</strong> {event._embedded.venues[0].name}
       </p>
@@ -35,6 +52,14 @@ const EventCard = ({ event }) => {
         {event._embedded.venues[0].city.name},{" "}
         {event._embedded.venues[0].state.stateCode}
       </p>
+      <button onClick={handleSelectAddress}>
+        {selectedAddress ? "Selected" : "Select"}
+      </button>
+      {selectedAddress && (
+        <p>
+          <strong>Selected Address:</strong> {selectedAddress}
+        </p>
+      )}
       <a href={event.url} target="_blank" rel="noopener noreferrer">
         More Info
       </a>
